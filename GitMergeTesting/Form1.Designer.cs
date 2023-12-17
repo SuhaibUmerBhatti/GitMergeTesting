b687c0b4003c17ed71e9cdd6b4fb4345b17b1358
SuhaibUmerBhatti <doctor_suhaib@hotmail.com>
2023-12-17 13:07:37 +0000

    Button adjusted

--- GitMergeTesting/Form1.Designer.cs
+++ GitMergeTesting/Form1.Designer.cs
@@ -28,19 +28,9 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(676, 12);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// textBox1
 			// 
@@ -49,6 +39,16 @@
 			this.textBox1.Size = new System.Drawing.Size(100, 22);
 			this.textBox1.TabIndex = 1;
 			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(688, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 38);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Do it!";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Form1
 			// 
@@ -65,9 +65,8 @@
 		}
 
 		#endregion
-
-		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
